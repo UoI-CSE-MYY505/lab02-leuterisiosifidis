8cec2a594a5cd93ec95dc0fddf653eb98ec2a0c8
Leuteris Iosifidis <cs05233@uoi.gr>
2024-10-19 18:35:05 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -9,10 +9,14 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a1, 7    # unsigned
     li a2, 1
 prog:
+    beq 7(a1) a2 done
+    
+    
 #-----------------------------
-# Write your code here!
+# Write your code here!hbkh
 # Do not remove the prog label or write code above it!
 #-----------------------------
+
 done:
     addi a7, zero, 10 
     ecall
