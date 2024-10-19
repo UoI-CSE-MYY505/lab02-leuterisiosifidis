81e689953588d8a7a161274ef821c44ca13f9d4e
Leuteris Iosifidis <cs05233@uoi.gr>
2024-10-19 18:38:11 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -8,15 +8,21 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     la a0, array
     li a1, 7    # unsigned
     li a2, 1
+###
 prog:
-    beq 7(a1) a2 done
-    
-    
-#-----------------------------
-# Write your code here!hbkh
-# Do not remove the prog label or write code above it!
-#-----------------------------
+findTheLast:
+    beq a1, zero, fail
+    slli s0, a1, 2
+    add s0 s0 a0 
+loop:
+    addi s0 s0 -4
+    lw t1 0(s0)
+    beq t1 a2 done 
+    bne s0 a0 loop
 
+fail:
+    add s0 zero zero
+###
 done:
     addi a7, zero, 10 
     ecall
